fc13c1385374a1c367b6db79098d6921c32bc00e
Harmin <harmin.parra@gmail.com>
2023-11-09 23:57:49 +0100

    increase version

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2023, Harmin Parra Rueda'
 author = 'Harmin Parra Rueda'
 
 # The short X.Y version
-version = '1.0'
+version = '1.1'
 # The full version, including alpha/beta/rc tags
 release = ''
 
